bebf4ca8e301b56a1e790e92ebc5305d8586b670
Nike Fedotov <nike.fedotoff@gmail.com>
2023-10-08 23:16:01 +0300

    Initial commit

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,112 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Никита on 08.10.2023.
+//
+
+import UIKit
+
+struct User {
+    let login: String
+    let password: String
+    let person: Person
+    
+    static func getUserData() -> User {
+        User(
+            login: "User",
+            password: "Password",
+            person: Person(name: "Nikita", surname: "Fedotov")
+        )
+    }
+}
+
+struct Person {
+    let name: String
+    let surname: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
+
+final class LoginViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    // MARK: - Private properties
+    private let user = User.getUserData()
+    
+    // MARK: Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let tabBarController = segue.destination as! UITabBarController
+        let welcomeVC = tabBarController.viewControllers?.first as! WelcomeViewController
+        welcomeVC.user = user
+
+    }
+    
+    // MARK: IBActions
+    @IBAction func logInPressed() {
+        guard
+            userNameTextField.text == user.login,
+            passwordTextField.text == user.password
+        else {
+            showAlert(title: "Invalid login or password",
+                      message: "Please, enter correct login and password",
+                      textField: passwordTextField)
+            return
+        }
+        
+        performSegue(withIdentifier: "logIn", sender: nil)
+    }
+    
+    @IBAction func forgotUserNamePressed() {
+        showAlert(title: "Oops!",
+                  message: "Your name is \(user.login) 😉")
+    }
+    
+    @IBAction func forgotPasswordPressed() {
+        showAlert(title: "Oops!",
+                  message: "Your password is \(user.password) 😉")
+    }
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        userNameTextField.text = nil
+        passwordTextField.text = nil
+    }
+    
+}
+
+// MARK: - Alert Controller
+extension LoginViewController {
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = nil
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
+// MARK: Text Field Delegate
+extension LoginViewController: UITextFieldDelegate {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameTextField {
+            passwordTextField.becomeFirstResponder()
+        } else {
+            logInPressed()
+        }
+        return true
+    }
+}
+
+
---
+++ LoginApp/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  LoginApp
+//
+//  Created by Никита on 08.10.2023.
+//
+
+import Foundation
